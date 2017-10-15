f02d8a4b6b2ada1bb83f0bb8287421058453ca46
divangani <hasanidivangani@gmail.com>
2017-10-15 21:56:18 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace sny
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frm_add_user  ());
+            Application.Run(new frm_home  ());
         }
     }
 }
